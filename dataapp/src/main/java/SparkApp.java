ab39c14937f311a2ee77dcc268d343bdf524d924
Mrunal Deshmukh <mrunalgaikwad29@gmail.com>
2020-06-11 18:39:12 -0700

    added field

--- dataapp/src/main/java/SparkApp.java
+++ dataapp/src/main/java/SparkApp.java
@@ -48,8 +48,8 @@ public class SparkApp {
         }
 
         @Override
-        /**                0                                ,1,             2       3, 4 ,5
-         * @param value 342a16a2-0235-42ac-ab1a-60da81daac60,Victor Scally,81416,2000,98933,7386
+        /**                0 ID                                ,   1 (NAME), 2(TAXAMOUNT) , 3(YEAR) , 4(ZIPCODE) ,5(NO OF HOUSEHOLDS)
+         * @param value 342a16a2-0235-42ac-ab1a-60da81daac60,Victor Scally,     81416,       2000,     98933,        7386
          */
         public String call(String value) throws Exception {
             String[] dataRowSplit = value.split(",");
@@ -61,6 +61,7 @@ public class SparkApp {
                             + cityAndData.getCity() + COMMA +
                             dataRowSplit[2] + COMMA +
                             dataRowSplit[3] + COMMA +
+                            dataRowSplit[4] + COMMA +
                             dataRowSplit[5];
                 }
             }
@@ -68,8 +69,6 @@ public class SparkApp {
         }
     }
 
-    //hdfs://localhost:9000/user/mrunaldeshmukh/datafiles/
-    //hdfs://localhost:9000/user/mrunaldeshmukh/spark-output
     public static void main(String[] args) throws ParseException, IOException {
         Options options = new Options();
         options.addOption("i", true, "provide hdfs url to read input data files");
